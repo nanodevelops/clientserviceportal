8c8bc5a2abf17f732f220866315cc0488f3d157f
Bareknuckles <dashkwesi@icloud.com>
2023-11-16 13:35:35 +0000

    Fixed issues with external DB connection

--- server/app.js
+++ server/app.js
@@ -4,19 +4,19 @@ const app = express()
 const cors = require("cors")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
 
-app.use(cors())
-app.use(bodyParser.json())
 
 mongoose.set("strictQuery", false)
-mongoose.connect(config.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+
+logger.info("Connecting to mongoDB Server:", config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI).then(() => {
     logger.info("Connected to MongoDB")
 }).catch((error) => {
     logger.error("Error connecting to external DB Server", error.message)
 })
+
+app.use(cors())
+app.use(express.json())
 
 module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
 const app = require("./app")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
-app.listen()
+app.listen(config.PORT, () => {logger.info(`Sever running on port ${config.PORT}`)})
