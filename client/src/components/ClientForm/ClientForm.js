47cf7c62adc61724443cc2724512ce4d6210cf7c
Bareknuckles <dashkwesi@icloud.com>
2024-01-09 18:49:26 +0000

    Add new clients feature with client form component

--- client/src/components/ClientForm/ClientForm.js
+++ client/src/components/ClientForm/ClientForm.js
@@ -1,76 +1,118 @@
-import { useState } from "react";
-import clientsService from "../../services/clients";
-import { useClientContext } from "../../ClientContext";
+import React from "react";
 
-const ClientForm = () => {
-    const { updateClients } = useClientContext;
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [contact, setContact] = useState("");
-    const [email, setEmail] = useState("");
-    const [visitPurpose, setVisitPurpose] = useState("");
-    const [entryTime, setEntryTime] = useState("");
-    const [exitTime, setExitTime] = useState("");
-    const [address, setAddress] = useState("");
-    const [gender, setGender] = useState("");
-    const [age, setAge] = useState("");
-    const [vulnerability, setVulnerability] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        const newClientObject = {
-            firstName,
-            lastName,
-            contact,
-            email,
-            visitPurpose,
-            entryTime,
-            exitTime,
-            address,
-            gender,
-            age,
-            vulnerability
-        }
-
-        clientsService.createClient(newClientObject).then(returnedClient => {
-            updateClients((previousClients) => [...previousClients, returnedClient])
-            setFirstName("")
-            setLastName("")
-            setContact("")
-            setEmail("")
-            setVisitPurpose("")
-            setEntryTime("")
-            setExitTime("")
-            setAddress("")
-            setGender("")
-            setAge("")
-            setVulnerability("")
-        })
-    }
+const ClientForm = ({ onFormSubmit, onFirstNameChange, firstNameValue, onLastNameChange, lastNameValue, onAgeChange, ageValue, onGenderChange, genderValue, onContactChange, contactValue, onEmailChange, emailValue, onAddressChange, addressValue, onVisitPurposeChange, visitPurposeValue, onEntryTimeChange, entryTimeValue, onExitTimeChange, exitTimeValue, onVulnerabilityChange, vulnerabilityValue }) => {
 
     return (
         <div className="client-form">
             <div className="container">
                 <h2>Add New Client</h2>
-                <form onSubmit={handleSubmit} className="row">
-                    <div className="col">
+                <form onSubmit={onFormSubmit} className="row g-2">
+                    <div className="col-md-3">
                         <label htmlFor="firstName" className="form-label">First Name</label>
                         <input 
                             type="text" required 
                             className="form-control" 
-                            value={firstName} 
-                            onChange={(e) => setFirstName(e.target.value)} 
+                            value={firstNameValue} 
+                            onChange={onFirstNameChange}
                         />
                     </div>
-                    <div className="col">
+                    <div className="col-md-3">
                         <label htmlFor="lastName" className="form-label">Last Name</label>
                         <input 
                             type="text" required 
                             className="form-control" 
-                            value={lastName} 
-                            onChange={(e) => setLastName(e.target.value)} 
+                            value={lastNameValue} 
+                            onChange={onLastNameChange} 
                         />
+                    </div>
+                    <div className="col-md-1">
+                        <label htmlFor="age" className="form-label">Age</label>
+                        <input 
+                            type="number" required
+                            className="form-control"
+                            value={ageValue}
+                            onChange={onAgeChange}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="gender" className="form-label">Gender</label>
+                        <select 
+                            name="gender" 
+                            id="gender"
+                            className="form-select"
+                            value={genderValue}
+                            onChange={onGenderChange}
+                        >
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                    <div className="col-md-3">
+                        <label htmlFor="contact" className="form-label">Contact</label>
+                        <input 
+                            type="text" required
+                            className="form-control" 
+                            value={contactValue}
+                            onChange={onContactChange}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={emailValue}
+                            onChange={onEmailChange}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="address" className="form-label">Address</label>
+                        <input 
+                            type="text" required
+                            className="form-control" 
+                            value={addressValue}
+                            onChange={onAddressChange}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="visitpurpose" className="form-label">Visit Purpose</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={visitPurposeValue}
+                            onChange={onVisitPurposeChange}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="entrytime" className="form-label">Entry Time</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={entryTimeValue}
+                            onChange={onEntryTimeChange}
+                        />
+                    </div>
+                    <div className="col-md-2">
+                        <label htmlFor="exittime" className="form-label">Exit Time</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={exitTimeValue}
+                            onChange={onExitTimeChange}
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <label htmlFor="vulnerability" className="form-label">Vulnerability</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            value={vulnerabilityValue}
+                            onChange={onVulnerabilityChange}
+                        />
+                    </div>
+                    <div className="col-md-12">
+                        <button type="submit" className="btn btn-primary">Add New Client</button>
                     </div>
                 </form>
             </div>
--- client/src/pages/AddNew/AddNew.js
+++ client/src/pages/AddNew/AddNew.js
@@ -1,10 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import ClientForm from "../../components/ClientForm/ClientForm";
+import clientsService from "../../services/clients";
+import { useClientContext } from "../../ClientContext";
 
 const AddNew = () => {
+    const {updateClients} = useClientContext;
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+    const [contact, setContact] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [visitPurpose, setVisitPurpose] = useState("");
+    const [entryTime, setEntryTime] = useState("");
+    const [exitTime, setExitTime] = useState("");
+    const [vulnerability, setVulnerability] = useState("");
+
+
+    const handleFirstNameChange = (e) => {
+        setFirstName(e.target.value)
+    }
+    const handleLastNameChange = (e) => {
+        setLastName(e.target.value)
+    }
+    const handleAgeChange = (e) => {
+        setAge(e.target.value)
+    } 
+    const handleGenderChange = (e) => {
+        setGender(e.target.value)
+    } 
+    const handleContactChange = (e) => {
+        setContact(e.target.value)
+    } 
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    } 
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value)
+    } 
+    const handleVisitPurposeChange = (e) => {
+        setVisitPurpose(e.target.value)
+    } 
+    const handleEntryTimeChange = (e) => {
+        setEntryTime(e.target.value)
+    } 
+    const handleExitTimeChange = (e) => {
+        setExitTime(e.target.value)
+    } 
+    const handleVulnerabilityChange = (e) => {
+        setVulnerability(e.target.value)
+    } 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const newClientObject = {
+            firstName,
+            lastName,
+            age,
+            gender,
+            contact,
+            email,
+            address,
+            visitPurpose,
+            entryTime,
+            exitTime,
+            vulnerability
+        }
+
+        clientsService.createClient(newClientObject).then(returnedClient => {
+            updateClients((previousClients) => [...previousClients, returnedClient])
+            setFirstName("")
+            setLastName("")
+            setContact("")
+            setEmail("")
+            setVisitPurpose("")
+            setEntryTime("")
+            setExitTime("")
+            setAddress("")
+            setGender("")
+            setAge("")
+            setVulnerability("")
+        })
+    }
+
     return ( 
         <>
-            <ClientForm />
+            <ClientForm 
+                onFormSubmit={handleSubmit}
+                onFirstNameChange={handleFirstNameChange}
+                onLastNameChange={handleLastNameChange}
+                onAgeChange={handleAgeChange}
+                onGenderChange={handleGenderChange}
+                onContactChange={handleContactChange}
+                onEmailChange={handleEmailChange}
+                onAddressChange={handleAddressChange}
+                onVisitPurposeChange={handleVisitPurposeChange}
+                onEntryTimeChange={handleEntryTimeChange}
+                onExitTimeChange={handleExitTimeChange}
+                onVulnerabilityChange={handleVulnerabilityChange}
+            />
         </>
      );
 }
