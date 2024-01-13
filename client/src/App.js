b566e40de8b28b096c26f3d5f15e1b146d42f5eb
Bareknuckles <dashkwesi@icloud.com>
2024-01-13 15:34:18 +0000

    The app now routes properly to /clients/view all url react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.scss";
 import Home from "./pages/Home";
 import AddNew from "./pages/AddNew/AddNew";
-// import ClientList from "./pages/ClientsList/ClientList";
+import Clients from "./pages/Clients.js/Clients";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
-      {/* <Route path="/clients" element={<ClientList />}/> */}
+      <Route path="/clients/view-all" element={<Clients />}/>
       <Route path="/clients/addnew" element={<AddNew />}/>
     </Routes>
   )
